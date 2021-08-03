e79e113210392cab08612a4bca9d62616dc0fe77
Milan Justel <milancjustel@gmail.com>
2021-08-02 23:34:22 -0700

    Add project files.

---
+++ RocketEvaluator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RocketEvaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Rocket
+    {
+
+    }
+}
+
+
+// Rockets      Mass    Thrust
+// Juno         1       4
+// Atlas        4       27
+// Soyuz        9       80
+// Saturn       20      200
